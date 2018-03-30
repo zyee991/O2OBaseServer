40b07c57bf6d9d19b3348d88238365fc1fce9a9b
zyee991 <zy991@foxmail.com>
2018-03-30 21:16:50 +0800

    3

---
+++ O2OBaseServer/src/main/java/com/o2o/util/CookieUtils.java
@@ -0,0 +1,76 @@
+package com.o2o.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookieUtils {
+	/**
+	 * 增加Cookie
+	 * 
+	 * @param response
+	 * @param name
+	 * @param value
+	 * @param maxAge
+	 */
+	public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+		Cookie cookie = new Cookie(name, value);
+		cookie.setPath("/");
+		if (maxAge > 0) {
+			cookie.setMaxAge(maxAge);
+		}
+		response.addCookie(cookie);
+	}
+	
+	/**
+	 * 根据名称获取值
+	 * @param request
+	 * @param name
+	 * @return
+	 */
+	public static String getCookie(HttpServletRequest request, String name){
+		Cookie cookie = getCookieByName(request, name);
+		if(cookie != null){
+			return cookie.getValue();
+		}
+		return "";
+	}
+
+	/**
+	 * 根据名字获取cookie
+	 * 
+	 * @param request
+	 * @param name
+	 *            cookie名字
+	 * @return
+	 */
+	public static Cookie getCookieByName(HttpServletRequest request, String name) {
+		Map<String, Cookie> cookieMap = ReadCookieMap(request);
+		if (cookieMap.containsKey(name)) {
+			Cookie cookie = (Cookie) cookieMap.get(name);
+			return cookie;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * 将cookie封装到Map里面
+	 * 
+	 * @param request
+	 * @return
+	 */
+	private static Map<String, Cookie> ReadCookieMap(HttpServletRequest request) {
+		Map<String, Cookie> cookieMap = new HashMap<String, Cookie>();
+		Cookie[] cookies = request.getCookies();
+		if (null != cookies) {
+			for (Cookie cookie : cookies) {
+				cookieMap.put(cookie.getName(), cookie);
+			}
+		}
+		return cookieMap;
+	}
+}
