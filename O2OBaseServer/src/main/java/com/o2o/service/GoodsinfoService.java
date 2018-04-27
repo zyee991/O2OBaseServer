84a6f3fcd1351064264cb3f229568ce75cc75795
Emily <Emily@DESKTOP-BOP4JON>
2018-04-28 00:37:21 +0800

    2

--- O2OBaseServer/src/main/java/com/o2o/service/GoodsinfoService.java
+++ O2OBaseServer/src/main/java/com/o2o/service/GoodsinfoService.java
@@ -1,47 +1,78 @@
 package com.o2o.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.Gson;
+import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
 import com.o2o.common.model.Goodsinfo;
 import com.o2o.common.model.Sectype;
+import com.o2o.common.model.Taocan;
 
 public class GoodsinfoService {
 
-	private static final Goodsinfo dao=new Goodsinfo().dao();
-	private static final Sectype typedao=new Sectype().dao();
+	private static final Goodsinfo dao = new Goodsinfo().dao();
+	private static final Sectype typedao = new Sectype().dao();
+	private static final Taocan taocanDao = new Taocan().dao();
+
 	// 查询商品
-	public  Page<Goodsinfo> paginate(int pageNumber,int pageSize){
-		return dao.paginate(pageNumber, pageSize, "select*","from tb_base_goodsinfo where goodsinfo_is_taocan = 0");
+	public Page<Goodsinfo> paginate(int pageNumber, int pageSize) {
+		return dao.paginate(pageNumber, pageSize, "select*", "from tb_base_goodsinfo where goodsinfo_is_taocan = 0");
 	}
-	
+
 	// 查询套餐
-	public  Page<Goodsinfo> paginateTaocan(int pageNumber,int pageSize){
-		return dao.paginate(pageNumber, pageSize, "select*","from tb_base_goodsinfo where goodsinfo_is_taocan = 1");
+	public Page<Goodsinfo> paginateTaocan(int pageNumber, int pageSize) {
+		return dao.paginate(pageNumber, pageSize, "select*", "from tb_base_goodsinfo where goodsinfo_is_taocan = 1");
 	}
-	
+
 	public Goodsinfo findById(String id) {
 		return dao.findById(id);
 	}
-	
+
 	public void deleteById(String id) {
 		dao.deleteById(id);
 	}
-	
-public void save(Goodsinfo goodsinfo) {
-		
-		try {
-			goodsinfo.save();	
-		} catch(Exception e) {
-			goodsinfo.update();	
-		}
-}
 
-   public List<Sectype> getTypeList(){
-	   List<Sectype> typelist= typedao.find("select a.* from tb_base_sectype a where a.first_type_id=1");
-		/*Gson gson=new Gson();
-		String str=gson.toJson(typelist);*/
-	     return typelist;
-   }
+	public void save(Goodsinfo goodsinfo) {
+
+		try {
+			goodsinfo.save();
+		} catch (Exception e) {
+			goodsinfo.update();
+		}
+	}
+
+	public List<Sectype> getTypeList() {
+		List<Sectype> typelist = typedao.find("select a.* from tb_base_sectype a where a.first_type_id=1");
+		/*
+		 * Gson gson=new Gson(); String str=gson.toJson(typelist);
+		 */
+		return typelist;
+	}
+
+	public List<Sectype> getTypeListForTaocanSelect() {
+		List<Sectype> typelist = typedao
+				.find("select a.* from tb_base_sectype a where a.first_type_id=1 and a.sec_type_id <> 0");
+		/*
+		 * Gson gson=new Gson(); String str=gson.toJson(typelist);
+		 */
+		return typelist;
+	}
+
+	public List<Goodsinfo> findByTypeId(String typeId) {
+		List<Goodsinfo> goodinfoList = dao
+				.find("select * from tb_base_goodsinfo where sec_type_id=? and goodsinfo_is_taocan=false", typeId);
+		return goodinfoList;
+	}
+
+	public List<Record> findTaocanByTaocanId(String taocanId) {
+		List<Record> taocanList = Db.find(
+				"select tg.*,g.goodsinfo_name,s.sec_type_name,s.sec_type_id from tb_base_taocan as tg join tb_base_goodsinfo"
+				+ " as g on tg.goodsinfo_id = g.goodsinfo_id join tb_base_sectype as s on"
+				+ " g.sec_type_id = s.sec_type_id where tg.taocan_id = ?",
+				taocanId);
+		return taocanList;
+	}
 }
--- O2OBaseServer/src/main/java/com/o2o/web/TaocanController.java
+++ O2OBaseServer/src/main/java/com/o2o/web/TaocanController.java
@@ -1,12 +1,15 @@
 package com.o2o.web;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import com.jfinal.core.Controller;
 import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
 import com.o2o.common.model.Goodsinfo;
 import com.o2o.common.model.Sectype;
+import com.o2o.common.model.Taocan;
 import com.o2o.service.GoodsinfoService;
 
 public class TaocanController extends Controller {
@@ -38,6 +41,10 @@ public class TaocanController extends Controller {
 		
 		List<Sectype> typelist = goodsinfoService.getTypeList();
 		setAttr("typelist", typelist);
+		
+		List<Record> taocanList = goodsinfoService.findTaocanByTaocanId(goodsinfo.getGoodsinfoId());
+		setAttr("detailList",taocanList);
+		System.out.println(taocanList);
 
 		render("update.html");
 	}
@@ -46,13 +53,12 @@ public class TaocanController extends Controller {
 	public void delete() {
 		String id = getPara("id");
 		goodsinfoService.deleteById(id);
-		renderJavascript("window.location.href='/goods_info'");
+		renderJavascript("window.location.href='/taocan'");
 	}
 
 	// 添加
 	public void add() {
-		List<Sectype> typelist = goodsinfoService.getTypeList();
-    System.out.println(typelist.toString());
+		List<Sectype> typelist = goodsinfoService.getTypeListForTaocanSelect();
 		setAttr("newId", UUID.randomUUID());
 		setAttr("typelist", typelist);
 		render("add.html");
@@ -62,20 +68,31 @@ public class TaocanController extends Controller {
 	public void save() {
 		Goodsinfo goodsinfo = getBean(Goodsinfo.class);
 		goodsinfoService.save(goodsinfo);
+		String goodsDetail = getPara("goodsDetail");
+		if(!"".equals(goodsDetail)) {
+			String[] goodsArray = goodsDetail.split(",");
+			for(String goods:goodsArray) {
+				if(!"".equals(goods)) {
+					Taocan taocan = new Taocan();
+					String taocanId = goodsinfo.getGoodsinfoId();
+					String goodsinfoId = goods.split(":")[0];
+					String itemCount = goods.split(":")[1];
+					String id = UUID.randomUUID().toString();
+					taocan.setTaoGoodId(id);
+					taocan.setTaocanId(taocanId);
+					taocan.setGoodsinfoId(goodsinfoId);
+					taocan.setItemCount(Integer.valueOf(itemCount));
+					taocan.save();
+				}
+			}
+		}
 		renderJavascript("window.location.href='/taocan'");
 	}
-
-	//
-	public void getTypeList() {
-		List<Sectype> typelist = goodsinfoService.getTypeList();
-		System.out.println(typelist.toString());
-		setAttr("typelist", typelist);
-		renderJson();
-	}
 	
-	public void getgoodsList(){
-		System.out.println(getPara("type_id"));
-		renderJson();
+	public void getGoods(){
+		String typeId = getPara("typeId");
+		List<Goodsinfo> list = goodsinfoService.findByTypeId(typeId);
+		renderJson(list);
 	}
 
 }
